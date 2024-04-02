9db788072f130dcd075990a863adee6d2ebf1349
Aakash Amod Rajput <126223325+aakash-a-dev@users.noreply.github.com>
2024-04-02 17:45:32 +0530

    Singup with database created

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,16 +1,39 @@
+import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
+const client = new PrismaClient();
 export function GET(req: NextRequest) {
     return NextResponse.json({
         email: "aakash@mail.com",
         name: "Aakash"
     })
 }
+// 
+
 
 export async function  POST(req: NextRequest) {
     const body = await req.json();
 
-    return NextResponse.json({
+    try {
+        await client.user.create({
+            data: {
+                username: body.username,
+                password: body.password
+            }
+            
+        });
+         return NextResponse.json({
         body
     })
+    } catch (error) {
+        NextResponse.json({
+            message: "Error while sign up"
+        }, {
+            status: 411
+        })
+    }
+
+
+    
+   
 }
